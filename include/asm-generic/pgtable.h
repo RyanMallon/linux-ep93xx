2a61c579b9c917b81fc71c45274d2e7b73cda23a
Rik van Riel <riel@redhat.com>
2012-10-09 15:31:12 +0200

    x86/mm: Introduce pte_accessible()
    We need pte_present to return true for _PAGE_PROTNONE pages, to indicate that
    the pte is associated with a page.
    
    However, for TLB flushing purposes, we would like to know whether the pte
    points to an actually accessible page.  This allows us to skip remote TLB
    flushes for pages that are not actually accessible.
    
    Signed-off-by: Rik van Riel <riel@redhat.com>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/n/tip-66p11te4uj23gevgh4j987ip@git.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/asm-generic/pgtable.h
+++ include/asm-generic/pgtable.h
@@ -206,6 +206,10 @@ static inline int pmd_same(pmd_t pmd_a, pmd_t pmd_b)
 #define move_pte(pte, prot, old_addr, new_addr)	(pte)
 #endif
 
+#ifndef __HAVE_ARCH_PTE_ACCESSIBLE
+#define pte_accessible(pte)		pte_present(pte)
+#endif
+
 #ifndef flush_tlb_fix_spurious_fault
 #define flush_tlb_fix_spurious_fault(vma, address) flush_tlb_page(vma, address)
 #endif
