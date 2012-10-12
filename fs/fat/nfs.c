49b9ff0dfa66e1595c0e20c09e6dfc98c27493cc
Andrew Morton <akpm@linux-foundation.org>
2012-10-12 15:23:02 +1100

    fat-exportfs-rebuild-inode-if-ilookup-fails-fix
    fix warnings/types
    
    fs/fat/nfs.c: In function 'fat_nfs_get_inode':
    fs/fat/nfs.c:68: warning: passing argument 3 of 'fat_get_blknr_offset' from incompatible pointer type
    fs/fat/fat.h:218: note: expected 'sector_t *' but argument is of type 'loff_t *'
    fs/fat/inode.c: In function '__fat_write_inode':
    fs/fat/inode.c:630: warning: passing argument 3 of 'fat_get_blknr_offset' from incompatible pointer type
    fs/fat/fat.h:218: note: expected 'sector_t *' but argument is of type 'loff_t *'
    
    Cc: "J. Bruce Fields" <bfields@fieldses.org>
    Cc: Amit Sahrawat <a.sahrawat@samsung.com>
    Cc: Namjae Jeon <namjae.jeon@samsung.com>
    Cc: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
    Cc: Ravishankar N <ravi.n1@samsung.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/fat/nfs.c
+++ fs/fat/nfs.c
@@ -63,7 +63,7 @@ static struct inode *fat_nfs_get_inode(struct super_block *sb,
 	if (inode == NULL && MSDOS_SB(sb)->options.nfs == FAT_NFS_NOSTALE_RO) {
 		struct buffer_head *bh = NULL;
 		struct msdos_dir_entry *de ;
-		loff_t blocknr;
+		sector_t blocknr;
 		int offset;
 		fat_get_blknr_offset(MSDOS_SB(sb), i_pos, &blocknr, &offset);
 		bh = sb_bread(sb, blocknr);
