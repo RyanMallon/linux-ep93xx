bc3d83ceeb7585a69c04435b249b35672a4fd2f9
Robin Holt <holt@sgi.com>
2013-05-23 10:36:52 +1000

    migrate-shutdown-reboot-to-boot-cpu-v11
    Signed-off-by: Robin Holt <holt@sgi.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/sys.c
+++ kernel/sys.c
@@ -370,19 +370,19 @@ EXPORT_SYMBOL(unregister_reboot_notifier);
 static void migrate_to_reboot_cpu(void)
 {
 	/* The boot cpu is always logical cpu 0 */
-	int reboot_cpu = 0;
+	int cpu = 0;
 
 	cpu_hotplug_disable();
 
 	/* Make certain the cpu I'm about to reboot on is online */
-	if (!cpu_online(reboot_cpu))
-		reboot_cpu = cpumask_first(cpu_online_mask);
+	if (!cpu_online(cpu))
+		cpu = cpumask_first(cpu_online_mask);
 
 	/* Prevent races with other tasks migrating this task */
 	current->flags |= PF_NO_SETAFFINITY;
 
 	/* Make certain I only run on the appropriate processor */
-	set_cpus_allowed_ptr(current, cpumask_of(reboot_cpu));
+	set_cpus_allowed_ptr(current, cpumask_of(cpu));
 }
 
 /**
